9e881b225224ca95ee5de06428102aa37a07a0cf
CSÉCSY László <laszlo.csecsy@pronovix.com>
2016-10-12 10:56:39 +0200

    Enable the new field instance on the default form display with more sane defaults

--- src/BrightcoveVideoField.php
+++ src/BrightcoveVideoField.php
@@ -25,18 +25,48 @@ class BrightcoveVideoField {
   public function createField($bundle) {
     $this->ensureFieldStorage();
 
+    // Create the field instance on this new media bundle.
     $field_config_storage = $this->entityTypeManager->getStorage('field_config');
     if (!$field_config_storage->load('media' . '.' . $bundle . '.' . BrightcoveVideo::FIELD_NAME)) {
       $field_config = $field_config_storage->create([
         'entity_type' => 'media',
         'field_name' => BrightcoveVideo::FIELD_NAME,
         'bundle' => $bundle,
+        'required' => TRUE,
+        'label' => 'Brightcove Video',
         'settings' => [
           'handler' => 'default:brightcove_video',
         ],
       ]);
       $field_config_storage->save($field_config);
     }
+
+    // Create (or update) the entity form display for this new media bundle to
+    // include this new field with some more sane defaults.
+    $entity_form_display_storage = $this->entityTypeManager->getStorage('entity_form_display');
+    $entity_form_display = $entity_form_display_storage->load('media.' . $bundle . '.default');
+    if (!$entity_form_display) {
+      $entity_form_display = $entity_form_display_storage->create([
+        'status' => TRUE,
+        'targetEntityType' => 'media',
+        'bundle' => $bundle,
+        'mode' => 'default',
+        'content' => [
+          BrightcoveVideo::FIELD_NAME => [
+            'type' => 'brightcove_inline_entity_form_complex',
+            'settings' => [
+              'form_mode' => 'default',
+              'allow_new' => 1,
+              'allow_existing' => 1,
+              'match_operator' => 'CONTAINS',
+            ],
+            'third_party_settings' => [],
+            'weight' => 0,
+          ],
+        ],
+      ]);
+    }
+    $entity_form_display_storage->save($entity_form_display);
   }
 
   protected function ensureFieldStorage() {
@@ -46,6 +76,9 @@ class BrightcoveVideoField {
         'entity_type' => 'media',
         'field_name' => BrightcoveVideo::FIELD_NAME,
         'type' => 'entity_reference',
+        'settings' => [
+          'target_type' => 'brightcove_video',
+        ],
       ]);
       $field_storage_config_storage->save($field_storage_config);
     }
